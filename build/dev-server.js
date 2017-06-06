933efac82b4c0716b78c50e7188a66ba18ccbd79
tanchengwei <804589369@qq.com>
2017-06-06 17:49:31 +0800

    first commit

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,11 +1,24 @@
 
 var webpackConfig = require("./webpack.dev.conf")
 var webpack = require("webpack")
+var webpackDevMiddleware = require("webpack-dev-middleware")
+var webpackHotMiddleware = require("webpack-hot-middleware")
+var express = require("express")
 var ora = require("ora")
 var chalk = require("chalk")
 
-var spinner = ora('building for production...')
-spinner.start()
 
-var compile = webpack(webpackConfig)
 
+var compiler = webpack(webpackConfig)
+
+var app = express()
+
+webpackDevMiddleware(compiler,{
+    publicPath:webpackConfig.output.publicPath
+});
+
+webpackHotMiddleware(compiler,{
+        log:function(){
+            console.log("log")
+        }
+});
